76273308510099d900d0f4cbe506694b28e2d5ca
HYNIX <hynix_1@naver.com>
2018-03-27 10:55:15 +0900

    망함wwwwwww

--- Association/script/script.js
+++ Association/script/script.js
@@ -1,7 +1,12 @@
 $(function () {
-    var i = 0;
+    $(".menu>li").hover(function () {
+        $(this).find("ul").stop().slideDown();
+    }, function () {
+        $(this).find("ul").stop().slideUp();
+    });
 
     function slide() {
+        var i = 0;
         i === 2 ? i = 0 : i++;
         $(".slide").animate({marginLeft: -i + "00%"}, 1000);
     }
